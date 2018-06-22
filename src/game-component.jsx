6e8402e2af4aa01b2721f439a99a2934cad83937
Matt Harrington <matt.harrington@cambiahealth.com>
2018-06-21 22:31:07 -0700

    adding win and lose conditions

--- src/game-component.jsx
+++ src/game-component.jsx
@@ -3,10 +3,16 @@ import PropTypes from 'prop-types';
 
 export default class GameComponent extends Component {
   render() {
+    const rickText = require(`./images/rick-text-${this.props.rickText}.png`)
+
     return (
       <div>
+        { this.props.isWin && <div className="overlay win"></div> }
+        { (this.props.isLose && !this.props.isWin) && <div className="overlay lose"></div> }
+
         <div className="hecklers">
-          <div className="rickText"></div>
+          <div className="rickText" style={{ backgroundImage: `url(${rickText})` }}></div>
+          <div className="moves">{ this.props.moves }</div>
         </div>
         <div className="boardContainer">
           {this.props.board.asComponent()}
@@ -17,5 +23,9 @@ export default class GameComponent extends Component {
 }
 
 GameComponent.propTypes = {
-  board: PropTypes.object.isRequired
+  board: PropTypes.object.isRequired,
+  rickText: PropTypes.number.isRequired,
+  moves: PropTypes.number.isRequired,
+  isWin: PropTypes.bool.isRequired,
+  isLose: PropTypes.bool.isRequired
 }
--- src/index.js
+++ src/index.js
@@ -9,20 +9,31 @@ const rootEl = document.getElementById('root');
 
 const map = [
   0, 20, 21, 13, 14, 20,  0,
-  0, 21, 13, 15, 11, 51,  0,
+  0, 21, 50, 15, 11, 51,  0,
   1, 10, 15, 20, 12, 10,  2,
-  0, 22, 50, 21, 23, 20,  0,
+  0, 22, 13, 21, 23, 20,  0,
   0, 20, 22, 20, 52, 23,  0,
 ]
 
 const board = new Board(3, map)
 
+let rickText
+
 board.observe(() => {
+  if (rickText === undefined) {
+    rickText = 1
+  } else {
+    rickText = Math.floor(Math.random() * 5) + 1
+  }
+
   ReactDOM.render(
-    <GameComponent board={board} />,
+    <GameComponent
+      board={board}
+      rickText={rickText}
+      moves={board.moves}
+      isWin={board.isWin()}
+      isLose={board.isLose()}
+    />,
     rootEl
   )
-  console.log(`moves = ${board.moves}`)
-  console.log(`isWin = ${board.isWin()}`)
-  console.log(`isLose = ${board.isLose()}`)
 })
